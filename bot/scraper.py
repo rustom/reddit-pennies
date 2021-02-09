d05ebe0e5e9e23aa7fc3d8bfac29accd4124b8d5
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2021-02-09 11:41:38 -0600

    Implement checking new posts with DD flair

---
+++ bot/scraper.py
@@ -0,0 +1,45 @@
+import asyncpraw
+import asyncio
+import discord
+import os
+
+channelID = 808500202109009920
+# subs = ['pennystocks']
+# temp_keywords = ['OCGN', 'DD', 'moon', 'invest', 'tomorrow', 'covid', 'the', ' ', 'trading', 'stock', 'penny']
+
+bar = '~~~~~~~~~~~~~~~~~~~~~~~~~~'
+
+token = os.environ['DISCORDBOT_TOKEN']
+username = os.environ['BOTONE_USERNAME']
+password = os.environ['BOTONE_PASSWORD']
+client_id = os.environ['BOTONE_ID']
+client_secret = os.environ['BOTONE_SECRET']
+user_agent = os.environ['BOTONE_AGENT']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(channelID)
+    await channel.send(f'{client.user} has connected to Discord on channel #{channel.name}!')
+
+    await scrape('pennystocks', channel)
+
+
+async def scrape(sub, channel):
+    posts = []
+
+    reddit = asyncpraw.Reddit(client_id=client_id, client_secret=client_secret, password=password, username=username, user_agent=user_agent)
+
+    subreddit = await reddit.subreddit(sub)
+    async for submission in subreddit.stream.submissions():
+        if submission.link_flair_text == 'DD :DD:':
+            await channel.send(bar + '\n' + '[' + submission.link_flair_text + ']' + submission.title + '\n' + submission.url + '/n/n')
+            
+# Criteria: 
+# - DD flair
+# - 300 upvotes
+# - 50 comments
+# - Last 5 hours
+
+client.run(token)
